16e12263557164f65ec8da1c5c12c30dae6460a6
Paolo Ragudo <ragudopaolo@gmail.com>
2025-06-05 00:15:37 +0800

    added logo and app bar shadow scroll

--- lib/home_page/home_screen.dart
+++ lib/home_page/home_screen.dart
@@ -11,18 +11,36 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        toolbarHeight: 80,
+        toolbarHeight: 100,
         centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        scrolledUnderElevation: 5,
+        shadowColor: Colors.black,
         title: Column(
           children: [
-            Text('Logo here'), // Replace with Image.asset('assets/logo.png')),
+            Image.asset(
+              'assets/logo.png',
+              width: 40,
+              height: 40,
+            ), // Replace with Image.asset('assets/logo.png')),
             SizedBox(height: 10),
             Text(widget.title),
           ],
         ),
+      ),
+      body: ListView(
+        children: [
+        ],
       ),
     );
   }
