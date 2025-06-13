1ae26b28a6542f4362648fc263f0b577099db186
Paolo Ragudo <ragudopaolo@gmail.com>
2025-06-13 18:08:15 +0800

    channel container template, floating button

---
+++ lib/home_page/channel_container.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class ChannelContainer extends StatefulWidget {
+  final String title;
+  final String description;
+
+  const ChannelContainer({
+    super.key,
+    required this.title,
+    required this.description,
+  });
+
+
+
+  @override
+  State<ChannelContainer> createState() => _ChannelContainerState();
+}
+
+class _ChannelContainerState extends State<ChannelContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 5,
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 85,
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                widget.description.length > 50
+                ? "${widget.description.substring(0, 50)}....."
+                : widget.description,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/home_page/home_screen.dart
+++ lib/home_page/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_daddy_app/home_page/channel_container.dart';
 
 class HomeScreen extends StatefulWidget {
   final String title;
@@ -24,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0,
-        scrolledUnderElevation: 5,
+        scrolledUnderElevation: 0,
         shadowColor: Colors.black,
         title: Column(
           children: [
@@ -37,11 +38,72 @@ class _HomeScreenState extends State<HomeScreen> {
             Text(widget.title),
           ],
         ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Container(
+            color: Colors.grey,
+            height: 1.0,
+          ),
+        ),
       ),
+      floatingActionButton: _addFloatingButton(
+        Colors.white,
+        () {}
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       body: ListView(
         children: [
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Channels",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18
+                  ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: ChannelContainer(
+                    title: 'General',
+                    description: 'General channel for 9Go Daddy friend group',
+                  ),
+                ),
+                SizedBox(height: 15),
+                Center(
+                  child: ChannelContainer(
+                    title: 'Summer Vacation',
+                    description: '2nd Year Summer Vacation',
+                  ),
+                ),
+                SizedBox(height: 15),
+                Center(
+                  child: ChannelContainer(
+                    title: 'Kumo Pics',
+                    description: 'Pictures of Kumo',
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
+    );
+  }
+
+  FloatingActionButton? _addFloatingButton(Color color, Function() callback) {
+    return FloatingActionButton(
+      backgroundColor: color,
+      child: Center(
+        child: Icon(Icons.add, color: Colors.black,),
+      ),
+      onPressed: () {
+        callback();
+      },
     );
   }
 }
